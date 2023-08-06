b4084b212082721eb7d22f45300b4afac723740a
saif.se <140830000+strombreake@users.noreply.github.com>
2023-08-06 19:24:19 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,5 @@
+class MyClass {       // The class
+    public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+};
